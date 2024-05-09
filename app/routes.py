a935207a9e1b17173b413078bef0894d1d68af52
Vagner Ferreira de Melo <165937363+Vagnermelo23@users.noreply.github.com>
2024-05-09 01:32:13 -0300

    uploud e download

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, request, jsonify, Blueprint
+from flask import render_template, request, jsonify, Blueprint, send_from_directory
 import json
+import os
 
 auth = Blueprint('auth', __name__)
 
@@ -26,3 +27,29 @@ def autenticar():
             if usuario['nome'] == nome and usuario['senha'] == senha:
                 return jsonify({'nome': nome, 'senha': senha})
         return "erro"
+
+@auth.route('/upload')
+def upload ():
+    return render_template('upload.html' )
+
+@auth.route('/autenticar2', methods=['POST']) 
+def autenticar2 ():
+    arquivo  = request.files.get('Documento')  
+    nome_arquivo = arquivo.filename.replace(" ","-")
+    arquivo.save(os.path.join('arquivos', nome_arquivo))
+    return f"arquivo {nome_arquivo} salvo com sucesso"
+
+@auth.route('/download')
+def download ():
+    arquivos = os.listdir('arquivos')
+    return render_template('download.html', arquivos=arquivos)
+import os
+
+@auth.route('/autenticar3', methods=['POST']) 
+def autenticar3():
+    nome_do_arquivo = request.form.get('download')
+    diretorio = 'arquivos'
+    caminho_arquivo = os.path.join(diretorio, nome_do_arquivo)
+    
+    return send_from_directory(diretorio, nome_do_arquivo, as_attachment=True)
+
